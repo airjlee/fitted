e6f5019f6c8e2f17528e51613addbe074e413304
Daniel Le <ledaniel0310@gmail.com>
2023-10-15 03:16:21 -0700

    Add files via upload

---
+++ DubHacks23v2App.swift
@@ -0,0 +1,17 @@
+//
+//  DubHacks23v2App.swift
+//  DubHacks23v2
+//
+//  Created by Daniel Le on 10/15/23.
+//
+
+import SwiftUI
+
+@main
+struct DubHacks23v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
